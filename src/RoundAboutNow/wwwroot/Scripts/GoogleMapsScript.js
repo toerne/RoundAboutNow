540dddae68fbab413bd65a1c4d842ae6bb532b3b
Kristoffer Törnblom <kristoffer.tornblom@gmail.com>
2016-11-29 10:50:08 +0100

    Added current position to H1

--- src/RoundAboutNow/wwwroot/Scripts/GoogleMapsScript.js
+++ src/RoundAboutNow/wwwroot/Scripts/GoogleMapsScript.js
@@ -11,13 +11,15 @@ function initMap() {
 
 function showGoogleMap(position) {
     var uluru = { lat: position.coords.latitude, lng: position.coords.longitude };
+    var geocoder = new google.maps.Geocoder;
+    geocodeLatLng(geocoder, uluru);
 
     map = new google.maps.Map(document.getElementById('googlemap'), {
         zoom: 14,
         center: uluru
     });
 
-    
+
     var imageUrl = "http://localhost:22416/images/youarehere.png";
 
     var image = {
@@ -46,5 +48,52 @@ function showGoogleMap(position) {
         center: uluru,
         radius: 500
     });
+
+
+
+    function geocodeLatLng(geocoder, uluru) {
+
+        geocoder.geocode({ 'location': uluru }, function (results, status) {
+            if (status === 'OK') {
+                console.log(results);
+
+                var areaName;
+
+                for (var i = 0; i < results.length; i++) {
+                    areaName = results[i].formatted_address;
+                    var breakloop = false;
+                    for (var j = 0; j < results[i].types.length; j++) {
+                        if (results[i].types[j] === "neighborhood" || results[i].types[j] === "sublocality") {
+                            breakloop = true;
+                            break;
+                        }
+                    }
+                    if (breakloop) {
+                        $("#areaTitle").text("Status för " + areaName.split(",")[0]);
+                        console.log(areaName.split(",")[0]);
+                        break;
+                    }
+                }
+                //$(results).each(function (index) {
+                //    areaName = this.formatted_address;
+                    
+                //    var breakloop = false;
+
+                //    $(this.types).each(function (index2) {
+                //        if (this === "neighborhood") {
+                //            breakloop = true;
+                //            return false;
+                //        }
+                //    });
+                //    if (breakloop === true)
+                //        return false;
+                //    console.log(index + ": " + this.formatted_address);
+                //});
+                //TODO: getelementbyid text = areaName
+            } else {
+                console.log('Geocoder failed due to: ' + status);
+            }
+        });
+    }
 }
 
